b56d4f711deb5f7d5ea04f9538d12dd1067246bf
Jen E <jeneisenberg5@gmail.com>
2013-06-24 23:02:06 -0400

    working on part 2 - no images

--- app.rb
+++ app.rb
@@ -1,10 +1,34 @@
 require 'sinatra/base'
+require_relative 'student-orm/lib/models/student'
+require 'sqlite3'
 
 # Why is it a good idea to wrap our App class in a module?
 module StudentSite
   class App < Sinatra::Base
+
     get '/' do
       "hello world!"
     end
+
+    get '/hello-world' do
+      @random_numbers = (1..20).to_a
+      erb :hello
+    end
+
+    get '/artists' do
+      @artists = ["James Taylor", "Frank Sinatra", "Louis Armstrong"]
+      erb :artists
+    end
+
+    get '/students' do
+      @students = Student.all
+      erb :students
+    end
+
+    get '/:name' do
+      @students = Student.all.find(params[:name])
+      erb :student_pro
+    end
+
   end
 end
